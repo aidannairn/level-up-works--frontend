1ba2a3ef5fdd276ff0bc716c29fb9dbb33396de3
aidannairn <aidan.nairn95@gmail.com>
2022-07-29 10:10:02 +1200

    feat: Add UserDashboard component.

---
+++ src/components/user-dashboard/UserDashboard.js
@@ -0,0 +1,16 @@
+import styled from "styled-components"
+
+const Container = styled.div`
+  background-color: #B2E4FA;
+  min-height: 100vh;
+`
+
+const UserDashboard = ({ items }) => {
+  return (
+    <Container>
+      
+    </Container>
+  )
+}
+
+export default UserDashboard
